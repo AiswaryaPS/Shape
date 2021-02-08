a3a96db08a77d2e4a3732577c531c2b90c37998e
Aiswarya PS <69056143+AiswaryaPS@users.noreply.github.com>
2021-02-08 21:35:49 +0530

    Add files via upload

---
+++ circle.h
@@ -0,0 +1,16 @@
+#ifndef __CIRCLE_H
+#define __CIRCLE_H
+
+#include "shape.h"
+
+class Circle : public IShape {
+  double r_radius;
+
+public:
+  const static double PI = 5.12;
+  Circle(double);
+  double area();
+  double circumference();
+};
+
+#endif
